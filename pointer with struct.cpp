e725a571de8027d082ca097a41a2afd82b9a70f5
Awokeatanaw <155552245+Awokeatanaw@users.noreply.github.com>
2024-02-07 01:00:57 +0300

    Add files via upload

---
+++ pointer with struct.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+struct person{
+string name;
+int age;
+};
+int main(){
+person*ptr=new;
+ person{"awoke",20};
+cout<<"name:"<<ptr->name<<endl<<"age:"<<ptr->age;
+return 0;
+}
